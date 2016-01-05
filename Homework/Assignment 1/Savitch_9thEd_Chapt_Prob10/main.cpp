40270276a63ff634c997d9612acaf0d96b3a204a
joses97 <jrs0925@gmail.com>
2016-01-05 11:55:54 -0800

    Signed-off-by: joses97 <jrs0925@gmail.com>

---
+++ Homework/Assignment 1/Savitch_9thEd_Chapt_Prob10/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Sanchez
+ * Purpose: BIG C
+ * Created on January 5, 2016, 11:40 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables 
+    
+    char letter='x';
+    
+     //Output
+    
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
