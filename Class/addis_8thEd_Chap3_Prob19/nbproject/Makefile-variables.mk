d695a24d9ea54f03c5040b63a7a09e28bc113739
joses97 <jrs0925@gmail.com>
2016-01-13 09:39:55 -0800

    Signed-off-by: joses97 <jrs0925@gmail.com>

--- Class/addis_8thEd_Chap3_Prob19/nbproject/Makefile-variables.mk
+++ Class/addis_8thEd_Chap3_Prob19/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=helloworld
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/helloworld
+CND_ARTIFACT_NAME_Debug=loan
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/loan
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=helloworld.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/helloworld.tar
+CND_PACKAGE_NAME_Debug=loan.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/loan.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=helloworld
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/helloworld
+CND_ARTIFACT_NAME_Release=loan
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/loan
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=helloworld.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/helloworld.tar
+CND_PACKAGE_NAME_Release=loan.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/loan.tar
 #
 # include compiler specific variables
 #
