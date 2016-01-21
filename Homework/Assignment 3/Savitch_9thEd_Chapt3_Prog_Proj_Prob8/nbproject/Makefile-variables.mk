3db48d38639ba1828544239b8853442fd3bf8cfa
joses97 <jrs0925@gmail.com>
2016-01-20 23:05:03 -0800

    Signed-off-by: joses97 <jrs0925@gmail.com>

--- Homework/Assignment 3/Savitch_9thEd_Chapt3_Prog_Proj_Prob8/nbproject/Makefile-variables.mk
+++ Homework/Assignment 3/Savitch_9thEd_Chapt3_Prog_Proj_Prob8/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=savitch_8thed_chap3_prob12
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/savitch_8thed_chap3_prob12
+CND_ARTIFACT_NAME_Debug=savitch_9thed_chapt3_prog_proj_prob8
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/savitch_9thed_chapt3_prog_proj_prob8
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=savitch8thedchap3prob12.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/savitch8thedchap3prob12.tar
+CND_PACKAGE_NAME_Debug=savitch9thedchapt3progprojprob8.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/savitch9thedchapt3progprojprob8.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=savitch_8thed_chap3_prob12
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/savitch_8thed_chap3_prob12
+CND_ARTIFACT_NAME_Release=savitch_9thed_chapt3_prog_proj_prob8
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/savitch_9thed_chapt3_prog_proj_prob8
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=savitch8thedchap3prob12.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/savitch8thedchap3prob12.tar
+CND_PACKAGE_NAME_Release=savitch9thedchapt3progprojprob8.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/savitch9thedchapt3progprojprob8.tar
 #
 # include compiler specific variables
 #
