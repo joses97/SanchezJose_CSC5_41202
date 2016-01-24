e2ce632358b34543369c4cb62ff6f1956cae4423
joses97 <jrs0925@gmail.com>
2016-01-23 16:06:24 -0800

    Signed-off-by: joses97 <jrs0925@gmail.com>

---
+++ Homework/Assignment 4/Gaddis_8thEd_Chapt5_Prob11/main.cpp
@@ -0,0 +1,58 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Sanchez
+ * Purpose: population increase
+ * Created on January 23, 2016
+ */
+
+//System Libraries
+#include <iostream> //I/O formating
+#include <iomanip>  //Formating 
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Problem to Solve
+    cout<<endl<<"Solution to Gaddis 8thEd Chap5 Prob11"<<endl;
+    cout<<"Hello this program predicts the population of an organism"<<endl;
+    //declare variables
+    int pop; //population
+    float dayIncr; //average daily population increase as a percent 
+    unsigned short numDays;    //number of days they will multiply for
+    unsigned short counter;    //count the number of days
+    
+    //input the initial population, daily population increase
+    cout<<"Please enter the initial population of the organism"<<endl;
+    cin>>pop;    //input initial population
+    cout<<"Please enter the daily population increase percent EX: 10"<<endl;
+    cin>>dayIncr;   //input daily increase as a percent
+    cout<<"Enter the number of days the population will grow for"<<endl;
+    cin>>numDays;//input number of days
+   
+    //calculations
+    dayIncr=dayIncr/100.0f; //convert day increase to a percent decimal form
+    
+    //statement to determine valid variables
+    if (pop>2&&dayIncr>=0&&numDays>1){
+         //loop to determine final population after numDays
+        do{
+            counter++;
+            numDays--;
+            pop=(pop*dayIncr)+pop;
+            cout<<"For day "<<counter<<" population is "<<pop<<endl;
+        }while(numDays!=0);
+    }else{
+        cout<<"Please enter a population larger than two"<<endl;
+        cout<<"A positive daily population increase"<<endl;
+        cout<<"And a number larger than one for the number of days"<<endl;
+    }
+        //Exit stage right and close
+    return 0;
+}
+
