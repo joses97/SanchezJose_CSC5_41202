20eb3debf4c3161d9bc201fef809b66a1bfb824e
joses97 <jrs0925@gmail.com>
2016-01-30 13:39:42 -0800

    Signed-off-by: joses97 <jrs0925@gmail.com>

---
+++ Project/CSC5Project/Project1/main.cpp
@@ -0,0 +1,29 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Jose Sanchez
+ * Purpose: 
+ * Created on January 22 2016, 2:00PM
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //output Hello World
+    cout<<"Hello World"<<endl;
+    
+    //Exit stage right and close
+    return 0;
+}
+
