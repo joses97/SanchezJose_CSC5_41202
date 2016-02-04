8c6b275b47797c1554d78b433e4b4343fa1cbd42
joses97 <jrs0925@gmail.com>
2016-02-03 16:53:34 -0800

    Signed-off-by: joses97 <jrs0925@gmail.com>

--- Class/Savitch_9thEd_Chap5_Pract_Prob2/main.cpp
+++ Class/Savitch_9thEd_Chap5_Pract_Prob2/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
@@ -16,41 +17,51 @@ using namespace std;
 //Global Constants
 
 //Function prototypes
-float input(int& feet, int& inches);
-float ftToMe(int& feet, int& inches);
-float ftToCm(int& feet, int& inches);
+int input(int& feet, int& inches); //user inputs the feet and inches
+int ftToMe(int feet, int inches); //converts feet and inches to meters and centimeters
+float meToCm(int feet, int inches);
+void output(int feet, int inches);
+
+
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables  
-    int feet=0, inches=0;
-    input(feet, inches);
-
+    int feet=0, inches=0; //feet and inches
+    
+    //set decimal place output to 3
+    cout<<fixed<<setprecision(2)<<showpoint<<endl;
+    cout<<"Savitch 9thEd Chapter 5 Problem 2"<<endl;
+    cout<<"This program will convert feet and inches to meters and centimeters"<<endl;
+    input(feet, inches); //user inputs the feet and inches
+    output(feet, inches);            //output the results
+    
+    
     
     //Exit stage right and close
     return 0;
 }
-//******************************************************************************
-//****************************Convert feet to inches****************************
-//******************************************************************************
-float ftToMe(int& feet, int& inches){
-    int inch=feet*12+inches;
-    int meter=inch/39.3701f;
-    return meter;
-    
+
+
+
+
+
+int input(int& feet, int& inches){
+    cout<<"Please input the feet and inches separated by a space"<<endl;
+    cin>>feet>>inches;
 }
-//******************************************************************************
-//****************************Convert inches to meters**************************
-//******************************************************************************
-float ftToCm(int& feet, int& inches){
-    int inch=feet*12+inches;
-    float meter=inch/39.3701f;
-    float centi=(meter-ftToMe(feet, inches))*100;
+int ftToMe(int feet, int inches){
+    int totImp=feet*12+inches;      //calculate total number of inches
+    int meters=totImp/39.3701f;   //calculates the number of meters
+    return meters;
+}
+float ftToCm(int feet, int inches){
+    float totImp=feet*12+inches;      //calculate total number of inches
+    float meters=totImp/39.3701f;   //calculates the number of meters
+    float centi=(meters-ftToMe(feet, inches))*100;
     return centi;
 }
-//******************************************************************************
-//****************************Convert inches to meters**************************
-//******************************************************************************
-float input(int& feet, int& inches){
-    cout<<"Input the feet and inches seperated by a space"<<endl;
-    cin>>feet>>inches;
+void output(int feet, int inches){
+    cout<<"You had "<<feet<<" feet and "<<inches<<" inches"<<endl;
+    cout<<"This converts to "<<ftToMe(feet, inches)<<" meters and "
+            ""<<ftToCm(feet, inches)<<" centimeters"<<endl;
 }
